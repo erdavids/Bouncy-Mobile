56a0ac2294ad663cb61088ca1f21a2590f26d228
erdavids <erdavids@ncsu.edu>
2020-11-18 23:19:56 -0500

    Second tutorial finished

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,12 @@ extends RigidBody2D
 var first_touch
 var release
 
+export var bounce_limit = 7
+
+var number_of_bounces = 0
+
+var debug = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (Input.is_action_just_pressed("Click")):
@@ -14,3 +20,12 @@ func _process(delta):
 		
 		linear_velocity = dir * delta * 90000
 		
+
+func _on_Player_body_entered(body):
+	number_of_bounces += 1
+	$Particles2D.emitting = true
+	
+	
+	if (number_of_bounces > bounce_limit and debug == false):
+		get_parent().remove_child(self)
+		queue_free()
